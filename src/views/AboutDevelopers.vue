b747c887eb8c32eff0044252b2620448a50ec8db
Oleg Shiliaev <shyliaievoleg@gmail.com>
2019-06-07 17:41:14 +0300

    check husky eslint on precommit

--- src/views/AboutDevelopers.vue
+++ src/views/AboutDevelopers.vue
@@ -1,5 +1,15 @@
 <template>
-    <section>
-        <h1>О разработчиках</h1>
-    </section>
+  <section>
+    <h1>О разработчиках</h1>
+    <ul>
+      <li>Alexandr Shleyko</li>
+      <li>Rostislav Shtanko</li>
+      <li>Danil Mogilniy</li>
+      <li>Max Mix</li>
+      <li>Irine Gazheva</li>
+      <li>Orlangure</li>
+      <li>Ilyas Hassen</li>
+      <li>Oleg Shiliaev</li>
+    </ul>
+  </section>
 </template>
