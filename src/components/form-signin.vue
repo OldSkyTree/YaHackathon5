4fc6b4235ef31dd1d8e3fb34324d4e097cbf1d55
Danil Mogilniy <danil_mogilniy@mail.ru>
2019-06-08 15:07:43 +0300

    client side auth

--- src/components/form-signin.vue
+++ src/components/form-signin.vue
@@ -65,8 +65,30 @@ export default {
 			const validation = this.$validate(this.user);
 
 			if(validation.isValid) {
-				// eslint-disable-next-line no-console
-				console.log('Форма заполнена без ошибок');
+				fetch('http://localhost:3000/login', {
+					method: 'POST',
+					body: JSON.stringify({
+						username: this.user.login.value,
+						password: this.user.password.value
+					}),
+					headers: { 'Content-type': 'application/json;charset=utf-8' }
+				})
+					.then(res => res.json())
+					.then(res => {
+						if(res.status !== 'error') {
+							localStorage.setItem('user', JSON.stringify(res));
+                        
+							this.$router.push({ path: '/profile' });
+						}
+						else {
+							// eslint-disable-next-line no-console
+							console.error('Error');
+						}
+					})
+					.catch(err => {
+						// eslint-disable-next-line no-console
+						console.error(err);
+					});
 			}
 			else {
 				this.errors = validation.errors;
